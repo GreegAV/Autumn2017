42118645cde6c1b2113126ee75eff4ac39cf14ed
GreegAV <greegav@gmail.com>
2017-10-27 23:22:09 +0300

    Lesson 2

---
+++ src/second/pack.java
@@ -0,0 +1,14 @@
+package second;
+
+/**
+ * Created by avg-m on 27/10/2017.
+ */
+public class pack {
+    public static void main(String[] args) {
+
+String i=" ";
+        if ( i != i )
+            System.out.println("Hi!");
+
+    }
+}
