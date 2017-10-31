e335baa6e7118e7bf67a7918c93a239f024dccbd
greegav <greegav@gmail.com>
2017-10-31 16:45:26 +0200

    Fin

--- src/third/Runner.java
+++ src/third/Runner.java
@@ -15,61 +15,84 @@ public class Runner {
 
         Scanner scn = new Scanner(System.in);
 
-        System.out.println("Введите горизонтальный размер матрицы: ");
-        int horSize = scn.nextInt();
+        System.out.println("Введите горизонтальный размер первой матрицы: ");
+        int horSize1 = scn.nextInt();
 
-        System.out.println("Введите вертикальный размер матрицы: ");
-        int vertSize = scn.nextInt();
+        System.out.println("Введите вертикальный размер первой матрицы: ");
+        int vertSize1 = scn.nextInt();
 
+        Matrix matr1 = MatrixFactory.createMatrix(horSize1, vertSize1);
         System.out.println("Матрица  1:");
-
-        Matrix matr1 = MatrixFactory.createMatrix(horSize, vertSize);
-
         System.out.println(matr1.toString());
+
+        System.out.println("Введите горизонтальный размер второй матрицы: ");
+        int horSize2 = scn.nextInt();
+
+        System.out.println("Введите вертикальный размер второй матрицы: ");
+        int vertSize2 = scn.nextInt();
+
+        Matrix matr2 = MatrixFactory.createMatrix(horSize2, vertSize2);
         System.out.println("Матрица 2:");
-
-        Matrix matr2 = MatrixFactory.createMatrix(horSize, vertSize);
-
         System.out.println(matr2.toString());
-//
-        System.out.println("Горизонтальный размер: " + matr1.getHorizontalSize());
-        System.out.println("Вертикальный размер: " + matr1.getVerticalSize());
 
-        System.out.println("Получение элемента по номеру строки и столбца");
+        System.out.println("Горизонтальный размер первой матрицы: " + matr1.getHorizontalSize());
+        System.out.println("Вертикальный размер первой матрицы: " + matr1.getVerticalSize());
 
-        boolean rightChoice = false;
-        int numCol, numRaw;
+        System.out.println("Горизонтальный размер второй матрицы: " + matr2.getHorizontalSize());
+        System.out.println("Вертикальный размер второй матрицы: " + matr2.getVerticalSize());
 
+        System.out.println("Получение элемента по номеру строки и столбца в первой матрице");
+
+
+        int numRaw = getNumRaw(scn, matr1);
+        int numCol = getNumCol(scn, matr1);
+
+        System.out.println("Искомый элемент: " + matr1.getElement(numRaw - 1, numCol - 1) + "\n");
+
+        System.out.println("Установка элемента матрицы");
+        int numRawSet = getNumRaw(scn, matr1);
+        int numColSet = getNumCol(scn, matr1);
+        System.out.println("Введите элемент для вставки :");
+        int insElement = scn.nextInt();
+
+        matr1.setElement(numColSet - 1, numRawSet - 1, insElement);
+
+        System.out.println("Результат вставки: ");
+        System.out.println(matr1.toString());
+
+        Matrix sumMatrix = Summator.sum(matr1, matr2);
+
+        if (sumMatrix != null) {
+            System.out.println("Сумма: ");
+            System.out.println(sumMatrix.toString());
+        }
+    }
+
+    private static int getNumCol(Scanner scn, Matrix matr1) {
+        boolean rightChoice;
+        int numCol;
         do {
-            System.out.println("Введите номер строки :");
-            numRaw = scn.nextInt();
-            if ((numRaw > 0) && (numRaw - 1 < matr1.getHorizontalSize())) {
+            rightChoice = false;
+            System.out.println("Введите номер столбца :");
+            numCol = scn.nextInt();
+            if ((numCol > 0) && (numCol - 1 < matr1.getHorizontalSize())) {
                 rightChoice = true;
             } else System.out.println("Введите число больше нуля и меньшее, чем горизонтальный размер матрицы");
         } while (!rightChoice);
+        return numCol;
+    }
 
-        rightChoice = false;
-
+    private static int getNumRaw(Scanner scn, Matrix matr1) {
+        boolean rightChoice;
+        int numRaw;
         do {
-            System.out.println("Введите номер столбца :");
-            numCol = scn.nextInt();
-            if ((numCol > 0) && (numCol - 1 < matr1.getVerticalSize())) {
+            rightChoice = false;
+            System.out.println("Введите номер строки :");
+            numRaw = scn.nextInt();
+            if ((numRaw > 0) && (numRaw - 1 < matr1.getVerticalSize())) {
                 rightChoice = true;
             } else System.out.println("Введите число больше нуля и меньшее, чем вертикальный размер матрицы");
         } while (!rightChoice);
-
-
-        System.out.println("Получение элемента: " + matr1.getElement(numRaw - 1, numCol - 1));
-//
-//        matr1.setElement(1, 1, 55);
-//
-//        System.out.println(matr1.toString());
-//
-//        Matrix sumMatrix = Summator.sum(matr1, matr2);
-//
-//        if (sumMatrix != null) {
-//            System.out.println("Сумма: ");
-//            System.out.println(sumMatrix.toString());
-//        }
+        return numRaw;
     }
 }
