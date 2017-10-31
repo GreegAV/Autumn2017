ede4418669514f6ae7ea652863eec74025fb44a3
greegav <greegav@gmail.com>
2017-10-31 14:08:35 +0200

    второй подход к штанге

--- shpota/matrix/MatrixFactory.java
+++
@@ -1,7 +0,0 @@
-package com.shpota.matrix;
-
-public class MatrixFactory {
-    public static Matrix createMatrix(int vertical, int horizontal) {
-        return new Matrix(vertical, horizontal);
-    }
-}
