800a73ee59603a0ebc739a1596161fc9dff15218
GreegAV <greegav@gmail.com>
2017-11-01 20:32:34 +0200

    finCar

--- src/les04/Car.java
+++ src/les04/Car.java
@@ -4,33 +4,50 @@ package les04;
  * Created by avg-m on 01/11/2017.
  */
 public class Car {
-    private  int carID;
-    private  String brand;
-    private  String model;
-    private  int year;
-    private  String color;
-    private  int price;
+    private int carID;
+    private String brand;
+    private String model;
+    private int year;
+    private String color;
+    private int price;
     private String regNum;
 
 
     public Car(int carID, String brand, String model, int year, String color, int price, String regNum) {
         this.carID = carID;
-        this.brand=brand;
-        this.model=model;
-        this.year=year;
-        this.color=color;
-        this.price=price;
-        this.regNum=regNum;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+        this.regNum = regNum;
     }
-    public String toString(){
-        String result="";
-        result+=carID+"\t";
-        result+=brand+"\t";
-        result+=model+"\t";
-        result+=year+"\t";
-        result+=color+"\t";
-        result+="$"+price+"\t";
-        result+=regNum;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String toString() {
+        String result = "";
+        result += carID + "\t";
+        result += brand + "\t";
+        result += model + "\t";
+        result += year + "\t";
+        result += color + "\t";
+        result += "$" + price + "\t";
+        result += regNum;
         return result;
     }
 }
--- src/les04/Runner.java
+++ src/les04/Runner.java
@@ -8,50 +8,63 @@ import java.util.Scanner;
  */
 public class Runner {
 
-    public static void brandSearch(String brand, ArrayList<Car> carList){
-        for (Car carT:carList) {
-            if(true) {
+    public static void brandSearch(String brand, ArrayList<Car> carList) {
+        for (Car carT : carList) {
+            if ( carT.getBrand().equals(brand) ) {
+                System.out.println("Find " + brand);
                 System.out.println(carT.toString());
             }
         }
-
-        System.out.println("brandsearchmethod");
     }
 
-    public static  void modelSearch (String model, int age, ArrayList<Car> carList){
-        System.out.println("modelsearchmethod");
+
+    public static void modelSearch(String model, int age, ArrayList<Car> carList) {
+
+        for (Car carT : carList) {
+            if ( carT.getModel().equals(model) ) {
+                if ( 2017 - carT.getYear() > age ) {
+                    System.out.println("Find " + model + " older than " + age + "years.");
+                    System.out.println(carT.toString());
+                }
+            }
+        }
     }
 
-    public static void yearSearch (int year, int price, ArrayList<Car> carList) {
-        System.out.println("yearsearchmethod");
+    public static void yearSearch(int year, int price, ArrayList<Car> carList) {
+        for (Car carT : carList) {
+            if ( carT.getYear() == year && carT.getPrice() == price ) {
+                System.out.println("Find year " + year + " with price more than " + price);
+                System.out.println(carT.toString());
+            }
+        }
     }
 
     public static void main(String[] args) {
-        int numCar=1;
-        ArrayList<Car>carList = new ArrayList<>();
+        int numCar = 1;
+        ArrayList<Car> carList = new ArrayList<>();
         carList.add(genCars(numCar));
 
         Scanner scn = new Scanner(System.in);
         System.out.println("Brand for searching: ");
-        String brandSearch=scn.nextLine();
+        String brandSearch = scn.nextLine();
         System.out.println("Model for searching: ");
-        String modelSearch=scn.nextLine();
+        String modelSearch = scn.nextLine();
         System.out.println("Age for searching: ");
-        int ageSearch=scn.nextInt();
+        int ageSearch = scn.nextInt();
         System.out.println("Year for searching: ");
-        int yearSearch=scn.nextInt();
+        int yearSearch = scn.nextInt();
         System.out.println("Price for searching: ");
-        int priceSearch=scn.nextInt();
+        int priceSearch = scn.nextInt();
 
         brandSearch(brandSearch, carList);
-        modelSearch(modelSearch,ageSearch,carList);
-        yearSearch(yearSearch,priceSearch,carList);
+        modelSearch(modelSearch, ageSearch, carList);
+        yearSearch(yearSearch, priceSearch, carList);
 
 
     }
 
     private static Car genCars(int numCar) {
-        Car car = new Car(numCar,"Chevrolet", "Aveo", 2000, "Red", 10000, "AA1111AA");
+        Car car = new Car(numCar, "Chevrolet", "Aveo", 2000, "Red", 10000, "AA1111AA");
         numCar++;
         return car;
     }
