0f21fcba5a604e7a47b3c62009b4075f968d7938
GreegAV <greegav@gmail.com>
2017-11-01 20:41:00 +0200

    finCar + nailing

--- src/les04/Runner.java
+++ src/les04/Runner.java
@@ -1,6 +1,9 @@
 package les04;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -8,41 +11,38 @@ import java.util.Scanner;
  */
 public class Runner {
 
-    public static void brandSearch(String brand, ArrayList<Car> carList) {
-        for (Car carT : carList) {
+    private static void brandSearch(String brand, ArrayList<Car> carList) {
+        for (Car carT : carList)
             if ( carT.getBrand().equals(brand) ) {
                 System.out.println("Find " + brand);
                 System.out.println(carT.toString());
             }
-        }
     }
 
-
-    public static void modelSearch(String model, int age, ArrayList<Car> carList) {
-
-        for (Car carT : carList) {
-            if ( carT.getModel().equals(model) ) {
-                if ( 2017 - carT.getYear() > age ) {
-                    System.out.println("Find " + model + " older than " + age + "years.");
+    private static void modelSearch(String model, int age, ArrayList<Car> carList) {
+        for (Car carT : carList)
+            if ( carT.getModel().toLowerCase().equals(model) ) {
+                if ( LocalDateTime.now().getYear() - carT.getYear() > age ) {
+                    System.out.println("Find " + model + " older than " + age + " years.");
                     System.out.println(carT.toString());
                 }
             }
-        }
     }
 
-    public static void yearSearch(int year, int price, ArrayList<Car> carList) {
-        for (Car carT : carList) {
+    private static void yearSearch(int year, int price, ArrayList<Car> carList) {
+        for (Car carT : carList)
             if ( carT.getYear() == year && carT.getPrice() == price ) {
                 System.out.println("Find year " + year + " with price more than " + price);
                 System.out.println(carT.toString());
             }
-        }
     }
 
     public static void main(String[] args) {
+
         int numCar = 1;
         ArrayList<Car> carList = new ArrayList<>();
         carList.add(genCars(numCar));
+        // TODO car generating
 
         Scanner scn = new Scanner(System.in);
         System.out.println("Brand for searching: ");
@@ -65,7 +65,6 @@ public class Runner {
 
     private static Car genCars(int numCar) {
         Car car = new Car(numCar, "Chevrolet", "Aveo", 2000, "Red", 10000, "AA1111AA");
-        numCar++;
         return car;
     }
 
