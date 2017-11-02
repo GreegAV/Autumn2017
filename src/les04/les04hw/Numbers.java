d4d1be46cc46f08e475b6c9e27876c7995d061c6
GreegAV <greegav@gmail.com>
2017-11-02 22:52:21 +0200

    les04 homework1

--- src/les04/les04hw/Numbers.java
+++ src/les04/les04hw/Numbers.java
@@ -8,20 +8,36 @@ import java.util.Scanner;
 public class Numbers {
     // 1. Дано целое число a. Определить число вхождений в него цифры b.
     public static void main(String[] args) {
-        Scanner scn= new Scanner(System.in);
-        System.out.println("Число-база: ");
-        long baseNumber=scn.nextLong();
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Число-база: ");
+        int baseNumber = scn.nextInt();
 
-        System.out.println("Число для поиска: ");
+        System.out.print("Цифра для поиска: ");
         int searchNumber = scn.nextInt();
-        while (searchNumber>9) {
-            System.out.println("Введите цифру от 0 до 9 включительно. "+searchNumber+">9.");
-             searchNumber = scn.nextInt();
-        }
-         while (baseNumber%searchNumber>10){
-             System.out.println(baseNumber%searchNumber);
-             baseNumber%=searchNumber;
-         }
 
+        while (searchNumber > 9 && searchNumber < 0) {
+            System.out.println("Введите цифру от 0 до 9 включительно. ");
+            System.out.print("Цифра для поиска: ");
+            searchNumber = scn.nextInt();
+        }
+        int enters = 0;
+        if ( baseNumber == searchNumber ) {
+            enters++;
+        }
+
+        int rest = baseNumber - (baseNumber / 10) * 10;
+        baseNumber /= 10;
+        System.out.println("rest :" + rest);
+        System.out.println("basenumber :" + baseNumber);
+        if ( rest == searchNumber ) {
+            enters++;
+        }
+
+        while (rest > 10) {
+            baseNumber /= 10;
+            rest
+        }
+
+        System.out.println("enters :" + enters);
     }
 }
