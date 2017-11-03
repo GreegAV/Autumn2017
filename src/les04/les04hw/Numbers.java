7e832fc2d096caffa7a12ff7566f7893107eb0b8
GreegAV <greegav@gmail.com>
2017-11-03 07:14:50 +0200

    les04 homework1

--- src/les04/les04hw/Numbers.java
+++ src/les04/les04hw/Numbers.java
@@ -15,29 +15,39 @@ public class Numbers {
         System.out.print("Цифра для поиска: ");
         int searchNumber = scn.nextInt();
 
-        while (searchNumber > 9 && searchNumber < 0) {
+        while (searchNumber > 9 || searchNumber < 0) {
             System.out.println("Введите цифру от 0 до 9 включительно. ");
             System.out.print("Цифра для поиска: ");
             searchNumber = scn.nextInt();
         }
         int enters = 0;
+        int rest=0;
+        if ( baseNumber == searchNumber ) {
+            enters++;
+        } else {
+
+//            rest = baseNumber - (baseNumber / 10) * 10;
+//            baseNumber /= 10;
+//            System.out.println("rest :" + rest);
+//            System.out.println("basenumber :" + baseNumber);
+//            if ( rest == searchNumber ) {
+//                enters++;
+//            }
+
+
+            do {
+                baseNumber /= 10;
+                rest = baseNumber - (baseNumber / 10) * 10;
+                System.out.println("rest :" + rest);
+                System.out.println("basenumber :" + baseNumber);
+                if ( rest == searchNumber  ) {
+                    enters++;
+                }
+            } while (baseNumber > searchNumber);
         if ( baseNumber == searchNumber ) {
             enters++;
         }
-
-        int rest = baseNumber - (baseNumber / 10) * 10;
-        baseNumber /= 10;
-        System.out.println("rest :" + rest);
-        System.out.println("basenumber :" + baseNumber);
-        if ( rest == searchNumber ) {
-            enters++;
         }
-
-        while (rest > 10) {
-            baseNumber /= 10;
-            rest
-        }
-
-        System.out.println("enters :" + enters);
+        System.out.println("Final enters :" + enters);
     }
 }
