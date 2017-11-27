adad51102ae865630654f5543a2aaec281c1e4f1
greegav <greegav@gmail.com>
2017-11-27 08:52:49 +0200

    cleanup

--- src/Training/JavaFX.java
+++ src/Training/JavaFX.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class JavaFX extends Application{
@@ -20,16 +21,26 @@ public class JavaFX extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Group group = new Group();
+        Scene scene = new Scene(group,300,400);
         window=primaryStage;
         window.setTitle("Hi!");
 
-//        Group group = new Group();
-//        Scene scene = new Scene(group,300,400);
+        JMenuBar menuBar = new JMenuBar();
+    JMenu editMenu = new JMenu("Edit");
+    menuBar.add(editMenu);
+
+        JMenuItem pasteItem = new JMenuItem("Paste");
+        editMenu.add(pasteItem);
+        editMenu.addSeparator();
+       // JMenu optionsMenu = ...; // Подменю.
+       // editMenu.add(optionsMenu);
+
 
 //        Menu fileMenu = new Menu("File");
 //        fileMenu.a
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 }
