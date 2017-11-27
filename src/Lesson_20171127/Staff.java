0414a264768a5890210a8defab3fab03f808ecb7
greegav <greegav@gmail.com>
2017-11-27 15:39:55 +0200

    file comparison char+byte complete

--- src/Lesson_20171127/Staff.java
+++ src/Lesson_20171127/Staff.java
@@ -23,10 +23,12 @@ public class Staff {
         int offsetDifference = 0;
         boolean different=false;
 
-        System.out.println("Выберите первый файл для сравнения : ");
+        System.out.print("Выберите первый файл для сравнения : ");
         File fname1 = getFileName();
-        System.out.println("Выберите второй файл для сравнения : ");
+        System.out.println(fname1.getName());
+        System.out.print("Выберите второй файл для сравнения : ");
         File fname2 = getFileName();
+        System.out.println(fname2.getName());
 
         try {
             Reader file1 = new BufferedReader(new FileReader(fname1));
@@ -38,6 +40,7 @@ public class Staff {
             if (filesize1 > 0 && filesize2 > 0) {
                 int c1 = file1.read();  //первый символ первого файла
                 int c2 = file2.read();  //первый симовл второго файла
+//                System.out.println((char) c1+" - "+(char) c2);
                 if (c1==c2){
                     offsetDifference++;
                     while ((c1 != -1) && (c2 != -1) && !different) {
@@ -51,12 +54,12 @@ public class Staff {
                         }
                     }
                 } else {
-                    // do nothing
+                    different=true;
                 }
             }
 
             System.out.print("- Результаты сравнения для файлов " + fname1 + " и " + fname2 + " :");
-            if (offsetDifference == 0) {
+            if (!different) {
                 System.out.println(" различий не обнаружено");
             } else {
                 System.out.println("\n- размеры файлов: " + fname1.getName() + " - " + filesize1 + " Байт, " + fname2.getName() + " - " + filesize2 + " Байт");
@@ -72,10 +75,12 @@ public class Staff {
         int offsetDifference = 0;
         boolean different=false;
 
-        System.out.println("Выберите первый файл для сравнения : ");
+        System.out.print("Выберите первый файл для сравнения : ");
         File fname1 = getFileName();
-        System.out.println("Выберите второй файл для сравнения : ");
+        System.out.println(fname1.getName());
+        System.out.print("Выберите второй файл для сравнения : ");
         File fname2 = getFileName();
+        System.out.println(fname2.getName());
 
         try {
             InputStream fstream1 = new BufferedInputStream(new FileInputStream(fname1));
@@ -87,6 +92,7 @@ public class Staff {
             if (filesize1 > 0 && filesize2 > 0) {
                 int c1 = fstream1.read();  //первый символ первого файла
                 int c2 = fstream2.read();  //первый символ второго файла
+//                System.out.println((char) c1+" - "+(char) c2);
                 if (c1==c2){
                     offsetDifference++;
                     while ((c1 != -1) && (c2 != -1) && !different) {
@@ -100,12 +106,12 @@ public class Staff {
                         }
                     }
                 } else {
-                    // do nothing
+                    different=true;
                 }
             }
 
             System.out.print("- Результаты сравнения для файлов " + fname1 + " и " + fname2 + " :");
-            if (offsetDifference == 0) {
+            if (!different) {
                 System.out.println(" различий не обнаружено");
             } else {
                 System.out.println("\n- размеры файлов: " + fname1.getName() + " - " + filesize1 + " Байт, " + fname2.getName() + " - " + filesize2 + " Байт");
