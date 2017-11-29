32ee0148e5a2b747b688f8285e2644c9b928d53d
GreegAV <greegav@gmail.com>
2017-11-29 19:33:21 +0200

    clean

--- src/Lesson_20171124/Runner.java
+++ src/Lesson_20171124/Runner.java
@@ -5,8 +5,6 @@ import static Lesson_20171124.TestExceptions.*;
 public class Runner {
     public static void main(String[] args) {
 
-//        TestExceptions testExceptions = new TestExceptions();
-
         System.out.println("Test NullPointerException");
         testException();
         System.out.println();
